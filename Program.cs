218b657af12dc70b6ecea0ce0a5ddcc8c7b8fc2a
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-09-27 13:56:51 +0200

    Övning metod

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+static void Main()
+{
+    int a = 10, b = 20;
+     BytPlats(ref a, ref b);
+    Console.WriteLine(a+" "+b);
+}
+
+static void BytPlats(ref int a, ref int b)
+{
+    int c = a;
+    a=b;
+    b=c;
+}
+Main();
+*/
+
+static void Main()
+{
+    Power(ref a, ref b);
+    Console.WriteLine(a+"upphöjt med"+b+"är"+ Power(a, b));
+}
